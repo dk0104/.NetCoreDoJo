5444b9bd4bb261ea44ce44340782aa1121f19d69
Denis <dk0104.dk@gmail.com>
2016-12-19 22:24:49 +0100

    Secon Step

---
+++ src/VierGewinnt/Owner.cs
@@ -0,0 +1,9 @@
+namespace VierGewinntLib
+{
+    enum Owner
+    {
+        RED,
+        GREEN,
+        VOID
+    }
+}
---
+++ src/VierGewinnt/Stone.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace VierGewinntLib
+{
+    public class Stone
+    {
+        #region [ Properties ]
+            
+        /// <summary>
+        /// Coordinat cotain x and y values. 
+        /// </summary>
+        /// <returns></returns>
+        Tuple<int,int> Coordinate {get; set;}
+        Owner CurrentOwner{get; set;}
+
+        #endregion
+
+        #region [Constructor]
+
+        public Stone(int horizintalCord, int verticalCoord)
+        {
+            Coordinate = new Tuple<int,int>(horizintalCord,verticalCoord);
+            CurrentOwner = Owner.VOID; 
+        }
+
+        #endregion
+    }
+}
